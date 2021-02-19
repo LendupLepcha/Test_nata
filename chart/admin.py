9ead6876d7363c66e487d4049b5848be6ebd5e9f
LendupLepcha <lenduplepcha62@gmail.com>
2021-02-19 12:33:46 +0530

    register models

--- chart/admin.py
+++ chart/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User_info, Zodiac, Aspects
+
+admin.site.register(User_info)
+admin.site.register(Zodiac)
+admin.site.register(Aspects)
+

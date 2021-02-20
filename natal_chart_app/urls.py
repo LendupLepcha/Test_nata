44aa55b4d4d6610303e55ebd0161a7e13acd5d56
LendupLepcha <lenduplepcha62@gmail.com>
2021-02-20 11:26:30 +0530

    register static

--- natal_chart_app/urls.py
+++ natal_chart_app/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
@@ -9,3 +9,5 @@ urlpatterns = [
     path('', views.homepage),
     path('chart/', include('chart.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()

bea5880c0f79bafe9039c1adfb75fd85ab2ba5e3
LendupLepcha <lenduplepcha62@gmail.com>
2021-02-24 15:20:55 +0530

    add static

--- natal_chart_app/urls.py
+++ natal_chart_app/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import static
 from . import views
 from chart import views as chart_view
 from django.conf import settings

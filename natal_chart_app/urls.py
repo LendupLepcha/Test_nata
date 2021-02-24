d816c1444099f0935d8409da0a262c65ae12e3cf
LendupLepcha <lenduplepcha62@gmail.com>
2021-02-24 11:50:53 +0530

    homepage redirect

--- natal_chart_app/urls.py
+++ natal_chart_app/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
-
+from chart import views as chart_view
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage),
+    path('', chart_view.view_create),
     path('chart/', include('chart.urls'))
 ]
 

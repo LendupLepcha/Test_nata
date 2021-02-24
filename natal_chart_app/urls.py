44b81d2e6c2744dbd4e7141e618d54ebe92667f8
LendupLepcha <lenduplepcha62@gmail.com>
2021-02-24 14:04:27 +0530

    last

--- natal_chart_app/urls.py
+++ natal_chart_app/urls.py
@@ -4,10 +4,15 @@ from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 from chart import views as chart_view
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', chart_view.view_create),
     path('chart/', include('chart.urls'))
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
